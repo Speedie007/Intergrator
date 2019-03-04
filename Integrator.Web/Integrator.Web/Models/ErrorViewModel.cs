1548a9b87a5cac53b7c87e113948b2ec4671d2c1
Brendan Wood <Brendanw@mweb.co.za>
2019-03-04 20:45:16 +0200

    Frot page

---
+++ Integrator.Web/Integrator.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Integrator.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
